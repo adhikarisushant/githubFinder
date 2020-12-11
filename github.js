c9a4a276c199824497b09eae3594ec068b82f886
Sushant <sushant.adhikari.619@gmail.com>
2020-12-11 17:40:04 +0545

    fetch display repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
     constructor() {
         this.client_id = '955ddd9dfd5d29fb6576';
         this.client_secret = '185e3ffd20f4153371ae654e6989beb9ba714d51';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
         
     }
